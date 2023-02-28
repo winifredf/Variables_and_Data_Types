da3eab50c098d4dfff6363658fc37e8a6ad61ef2
Winifredf <Winifred.Dykes@hotmail.com>
2023-02-28 18:02:26 -0500

    Created name and age variables

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+character_name ="Belfar"
+character_age = "19"
+
 print("Once upon a time there was a princess whose name was Belfar.")
 print("She was 19 years of age.")
 print("She loved living life to the fullest at 19,")
