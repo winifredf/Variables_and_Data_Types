58aa791855d32293c8fac4a49295bcb360bded5e
Winifredf <Winifred.Dykes@hotmail.com>
2023-02-28 18:16:08 -0500

    inserted variables for name and age into program instead of hard coding values

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 character_name ="Belfar"
 character_age = "19"
 
-print("Once upon a time there was a princess whose name was Belfar.")
-print("She was 19 years of age.")
-print("She loved living life to the fullest at 19,")
-print("but she despised the name Belfar.")
-print("Princess Belfar set out on a mission to change her name")
+print("Once upon a time there was a princess whose name was " + character_name + ".")
+print("She was " + character_age + " years of age.")
+print("She loved living life to the fullest at " + character_name + ",")
+print("but she despised the name " + character_name + ".")
+print("Princess " + character_name + " set out on a mission to change her name")
 print("She wanted to choose a name that had meaning,")
 print("and represented who she was as a person.")
-print("Princess Belfar loves being 19 and her new meaningful name!")
+print("Princess " + character_name + " loves being " + character_age + " and her new meaningful name!")
